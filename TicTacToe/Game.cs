208eb522e87e9b4e600a5cb28aba95bbd39eb326
NateMG4 <natemg4@outlook.com>
2023-05-25 13:43:16 -0700

    Create GameRunner class to comunicate betwen board view and game

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Net.Sockets;
 using System.Runtime.CompilerServices;
+using System.Transactions;
 
 namespace TicTacToe;
 
@@ -9,40 +10,53 @@ public class Game
     public Player[] players { get; private set; } = new Player[2];
     public BoardModel model { get; private set;}
     public GameRunner runner { get; private set; }
+
+    public Player currentPlayer { get; private set; }
     BackgroundWorker turnWorker = new BackgroundWorker();
 
-    public Game(GameRunner runner)
+    public Game(GameRunner runner, List<PlayerType> types)
     {
-        model = new BoardModel(this);
+        InitializeBackgroundWorker();
+
+        model = new BoardModel();
+        players[0] = Player.create(types[0],1);
+        players[1] = Player.create(types[1], -1);
+        currentPlayer = players[0];
         this.runner = runner;
         turnWorker.RunWorkerAsync();
-        this.runner = runner;
     }
 
     private async void GameLoop(object sender, DoWorkEventArgs e)
     {
+        BackgroundWorker worker = sender as BackgroundWorker;
         while (!model.gameFinished)
         {
-            Player currentPlayer = players[model.playerNumber];
-            currentPlayer.startTurn(model);
-
-            int move = currentPlayer.getMove();
-            if (move != -1)
+            currentPlayer = players[model.playerNumber];
+            if (currentPlayer.startTurn(model))
             {
+                int move = currentPlayer.getMove();
+
                 model.move(move);
                 model.checkIfTerminalState();
+                worker.ReportProgress(model.turn / 9);
             }
+
+            // await Task.Delay(500);
+
         }
     }
 
     private async void GameFinished(object sender, RunWorkerCompletedEventArgs e)
     {
+        runner.GameFinished();
 
     }
-    private void GameProgress(object sender, ProgressChangedEventArgs e)
+    private void TurnFinished(object sender, ProgressChangedEventArgs e)
     {
+        runner.TurnFinished();
 
     }
+
     private void InitializeBackgroundWorker()
     {
         this.turnWorker.WorkerReportsProgress = true;
@@ -57,7 +71,7 @@ public class Game
 
         turnWorker.ProgressChanged +=
             new ProgressChangedEventHandler(
-                GameProgress);
+                TurnFinished);
 
     }
 }
---
+++ TicTacToe/GameRunner.cs
@@ -0,0 +1,96 @@
+﻿using System.Reflection;
+
+namespace TicTacToe;
+
+public abstract class GameRunner
+{
+    public Game currentGame { get; protected set; }
+    public GameRunner()
+    {
+
+    }
+
+    public void CreateGame()
+    {
+        currentGame = new Game(this, getPlayerTypes());
+    }
+
+    public abstract List<PlayerType> getPlayerTypes();
+    public abstract void TurnFinished();
+    public abstract void GameFinished();
+    public abstract void reset();
+}
+
+public class TestRunner : GameRunner
+{
+    private int numGames = 0;
+    public bool finished = false;
+    public TestRunner(int numGames) : base()
+    {
+        this.numGames = numGames;
+    }
+    public override void GameFinished()
+    {
+        numGames--;
+        if (numGames >= 0)
+        {
+            reset();
+        }
+        else
+        {
+            finished = true;
+        }
+    }
+
+    public override List<PlayerType> getPlayerTypes()
+    {
+        return new List<PlayerType>() { PlayerType.AI_PLAYER, PlayerType.AI_PLAYER };
+    }
+
+    public override void reset()
+    {
+        CreateGame();
+    }
+
+    public override void TurnFinished()
+    {
+        
+    }
+}
+
+public class ContinuousGameRunner : GameRunner
+{
+    private BoardView viewer;
+
+    public ContinuousGameRunner(BoardView viewer) : base()
+    {
+        this.viewer = viewer;
+        CreateGame();
+    }
+
+    public override List<PlayerType> getPlayerTypes()
+    {
+        return viewer.getSelectedPlayerTypes();
+    }
+
+    public override void TurnFinished()
+    {
+        viewer.updateFromModel(currentGame.model);
+    }
+
+    public override void GameFinished()
+    {
+        viewer.updateFromModel(currentGame.model);
+    }
+
+    public override void reset()
+    {
+        CreateGame();
+        viewer.resetBoard(currentGame.model);
+    }
+
+    public void CellClicked(int move)
+    {
+        currentGame.currentPlayer.setMove(move);
+    }
+}
