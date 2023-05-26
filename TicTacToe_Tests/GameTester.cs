dbf213b607e83cf3b6dfde77bf299be0c5efa0bc
NateM <natemg4@outlook.com>
2023-05-26 10:49:44 -0700

    Created unit tests for BoardModel and the AI players

---
+++ TicTacToe_Tests/GameTester.cs
@@ -0,0 +1,28 @@
+﻿using GameLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TestProject1
+{
+    [TestClass]
+    public class GameTester
+    {
+
+        [TestMethod]
+        public void AI_Draw_Test()
+        {
+            int Games_To_Play = 1000;
+            TestRunner tester = new TestRunner(Games_To_Play);
+            tester.StartTest();
+
+            while (!tester.finished)
+            {
+                tester.Run();
+            }
+
+            Assert.AreEqual(0, tester.record);
+        }
+    }
+}
---
+++ TicTacToe_Tests/ModelTest.cs
@@ -0,0 +1,78 @@
+using GameLogic;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class ModelTest
+    {
+        [TestMethod]
+        public void TestBlank()
+        {
+            BoardModel board = new BoardModel();
+            Assert.AreEqual(0, board.turn);
+            Assert.AreEqual(0, board.playerNumber);
+            Assert.AreEqual(1, board.playerID);
+            Assert.AreEqual(0, board.gameState);
+            Assert.IsFalse(board.gameFinished);
+            
+
+
+            for (int i = 0; i < 9; i++)
+            {
+                Assert.AreEqual(0,board.getCellState(i));
+            }
+           
+
+        }
+        [TestMethod]
+        public void TestFirstMove()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                BoardModel board = new BoardModel();
+
+                board.move(i);
+                Assert.AreEqual(1, board.getCellState(i));
+            }
+
+
+        }
+
+        [TestMethod]
+        public void TestSecondMove()
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                BoardModel board = new BoardModel();
+                board.move(0);
+                board.move(i);
+                Assert.AreEqual(-1, board.getCellState(i));
+            }
+        }
+
+        [TestMethod]
+        public void InvalidMove()
+        {
+            BoardModel board = new BoardModel();
+            board.move(-1);
+            Assert.AreEqual(0, board.turn);
+            board.move(10);
+            Assert.AreEqual(0, board.turn);
+        }
+
+        [TestMethod]
+        public void OccupiedMove()
+        {
+            BoardModel board = new BoardModel();
+            board.move(0);
+            Assert.AreEqual(1, board.turn);
+            Assert.AreEqual(1, board.getCellState(0));
+
+            board.move(0);
+            Assert.AreEqual(1, board.turn);
+            Assert.AreEqual(1, board.getCellState(0));
+
+        }
+
+    }
+}
